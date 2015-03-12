8c77a1ac3e28cc38d228efd80bfd2722226afb15
FerBayley <fernando.bayley@gmail.com>
2015-03-12 02:57:58 -0300

    Priemera maquetacion

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Detali Sastreria Italiana</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="css/main.css" />
+	<!-- CSS -->
+
+</head>
+<body>
+
+	<header>
+		<nav>
+			<ul>
+				<li><a href="#">INICIO</a></li>
+				<li><a href="#">EVENTOS</a></li>
+				<li><a href="#">CONDICIONES</a></li>
+				<li><a href="#">A MEDIDA</a></li>
+				<li><a href="#">QUIEN SOY</a></li>
+				<li><a href="#">FORMAS DE PAGO</a></li>
+				<li><a href="#">CONTACTO</a></li>
+			</ul>
+		</nav> <!-- End of Nav -->
+	</header> <!-- End of Header -->
+
+
+
+	<section class="Contenido-principal">	
+		<section class="Columna-menu">
+			<h1>
+				<a href="#">
+				</a>
+			</h1>
+
+			<ul>
+				<li><img src="img/logo.png" alt="Detali Sastreria Italiana"></li>
+				<li><a href="#">TRAJES</a></li>
+				<li><a href="#">CAMISAS</a></li>
+				<li><a href="#">ABRIGOS</a></li>
+				<li><a href="#">PANTALONES</a></li>
+				<li><a href="#">ZAPATOS</a></li>
+				<li><a href="#">CINTURONES</a></li>
+				<li><a href="#">ACCESOIOS</a></li>
+			</ul>
+		</section> <!-- End of Columna-menu -->
+
+		<section class="Redes">
+			<ul>
+				<li><a href="#"><img src="img/facebook.png" alt="Facebook"></a></li>
+				<li><a href="#"><img src="img/twitter.png" alt="Seguinos en Twitter"></a></li>
+				<li><a href="#"><img src="img/pinterest.png" alt="Mira nuestras imagenes en Pinterest"></a></li>
+				<li><a href="#"><img src="img/instagram.png" alt="Instagram"></a></li>
+			</ul>
+		</section> <!-- End of Redes -->
+
+		<footer>
+			<p>
+				Derechos reservados 2015  -  Av Roque Saenz Peña 615  -  
+				Piso 5 Oficinas 512 - Tel: 4-328-9656 / <a href="mailto:sastreria@detalil.com.ar">
+					sastreria@detali.com.ar
+				</a>
+			</p>
+		</footer> <!-- End of Footer -->
+	</section> <!-- End of Contenido-principal -->
+	
+</body>
+</html>
---
+++ pagos.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Formas de pago - Detali Sastreria Italiana</title>
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/normalize.css" />
+	<link rel="stylesheet" href="css/main.css" />
+	<!-- CSS -->
+
+</head>
+<body>
+
+	<header>
+		<nav>
+			<ul>
+				<li><a href="#">INICIO</a></li>
+				<li><a href="#">EVENTOS</a></li>
+				<li><a href="#">CONDICIONES</a></li>
+				<li><a href="#">A MEDIDA</a></li>
+				<li><a href="#">QUIEN SOY</a></li>
+				<li><a href="#">FORMAS DE PAGO</a></li>
+				<li><a href="#">CONTACTO</a></li>
+			</ul>
+		</nav> <!-- End of Nav -->
+	</header> <!-- End of Header -->
+
+	
+	<section class="Contenedor-formasDepago">
+		<div class="Contenedor-menu">
+			<div class="Imagen">
+				<img src="img/logo.png" alt="">
+			</div> <!-- End of Imagen -->
+
+			<div class="Menu-internas">
+				<ul>
+					<li><a href="">TRAJES</a></li>
+					<li><a href="">CAMISAS</a></li>
+					<li><a href="">PANTALONES</a></li>
+					<li><a href="">ZAPATOS</a></li>
+					<li><a href="">CORBATAS</a></li>
+					<li><a href="">CINTURONES</a></li>
+					<li><a href="">ACCESORIOS</a></li>
+				</ul>
+			</div> <!-- Menu -->
+		</div> <!-- Contenedor-menu -->
+	</section> <!-- End of Contenedor-zapatos -->
+
+	<section class="Cuerpo">
+		<h2>Formas de pago</h2>
+	</section> <!-- End of Cuerpo -->
+
+
+	<section class="Contenido">
+		contenido
+	</section> <!-- End of Contenido -->
+
+	<section class="Pie">
+		<div class="Tarjetas">
+			<img src="img/tarjetas.jpg" alt="">
+		</div> <!-- End of Tarjetas -->
+
+		<div class="Redes-pie">
+			<ul>
+				<li><a href=""><img src="img/facebook-interna.png" alt=""></a></li>
+				<li><a href=""><img src="img/twitter-interna.png" alt=""></a></li>
+				<li><a href=""><img src="img/pinterest-interna.png" alt=""></a></li>
+				<li><a href=""><img src="img/instagram-interna.png" alt=""></a></li>
+			</ul>
+		</div> <!-- End of Redes-pie -->
+
+		<div class="Info">
+			<p>
+				Derechos reservados 2015  -  Av Roque Saenz Peña 615  -  Piso 5 Oficinas 512 - 
+				Tel: 4 328 9656 - sastreriadetali@detali.com
+			</p>
+		</div> <!-- End of Info -->
+	</section> <!-- End of Pie -->
+	
+</body>
+</html>
